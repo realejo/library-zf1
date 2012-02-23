8e1f1390c0cc7b2414cea0beaac3bf729ef08cec
Rodrigo Capilé <rcapile@gmail.com>
2012-02-23 22:04:05 +0000

    View Helper para imprimir o tempo usando o RW_Time
    git-svn-id: http://svn.rw/realejo/rw/trunk@21 b1590d42-23a6-48ac-abc1-97985918d4f4

---
+++ library/RW/View/Helper/PrintTime.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Imprime as horas a partir dos segundos
+ *
+ * @category   RW
+ * @package    RW_View
+ * @subpackage Helper
+ * @author     Realejo
+ * @version    $Id$
+ * @copyright  Copyright (c) 2011-2012 Realejo Design Ltda. (http://www.realejo.com.br)
+ *
+ * @uses RW_Time
+ */
+class RW_View_Helper_PrintTime extends Zend_View_Helper_Abstract
+{
+
+    /**
+     * Imprime um tempo
+     *
+     * @param string|int|RW_Time $segundos Segundos ou objeto RW_Time
+     * @param string      $format    OPCIONAL Formato de saida
+     *
+     * @return string
+     */
+    public function printTime($time, $format = null)
+    {
+        if (!($time instanceof RW_Time)) $time = new RW_Time($time);
+        return $time->toString($format);
+    }
+}
