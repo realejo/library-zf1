3e350ba1ea4e85130e09d9ef8579eaef3605f07a
Rodrigo Capilé <rcapile@gmail.com>
2012-04-24 15:48:45 +0000

    Nova Versão
    git-svn-id: http://svn.rw/realejo/rw/trunk@24 b1590d42-23a6-48ac-abc1-97985918d4f4

--- library/RW/Version.php
+++ library/RW/Version.php
@@ -13,7 +13,7 @@ class RW_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    CONST VERSION = '1.0.0';
+    CONST VERSION = '1.0.1';
 
     /**
      * The latest stable version Zend Framework available
